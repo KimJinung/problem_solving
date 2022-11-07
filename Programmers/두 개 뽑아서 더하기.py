02c99dd29f81aba90986e38538c2f976a1795fc9
Jinung Kim <111354195+KimJinung@users.noreply.github.com>
2022-11-07 16:57:44 +0900

    2022.11.07.

---
+++ Programmers/두 개 뽑아서 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = set()
+    length = len(numbers)
+    
+    for i in range(length):
+        for j in range(i+1, length):
+            answer.add(numbers[i] + numbers[j])
+    
+    return sorted(list(answer))
