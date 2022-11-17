84d683d75402d0d57f46599c35e7b9cfdd65eea6
KimJinung <KimJinung@outlook.com>
2022-11-17 16:03:48 +0900

    2022.11.17.

---
+++ Programmers/Lv. 0/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    item = 2
+    
+    while n != 1:
+        if n % item == 0:
+            answer.append(item)
+            n /= item
+        else:
+            item += 1
+        
+    return sorted(list(set(answer)))
---
+++ Programmers/Lv. 0/영어가 싫어요.py
@@ -0,0 +1,29 @@
+def solution(numbers):
+    numbers_dict = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+    
+    answer = ''
+    num_str = ''
+    
+    for ch in numbers:
+        num_str += ch
+        
+        value = numbers_dict.get(num_str)
+        
+        if value:
+            answer += str(value)
+            num_str = ''
+            
+            
+    
+    return int(answer)
