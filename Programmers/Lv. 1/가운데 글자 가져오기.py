3e409c0faba3ea65b8e14e614d7c9cf59a6d4b3b
KimJinung <KimJinung@outlook.com>
2022-11-28 14:20:33 +0900

    Apply black formatter

--- Programmers/Lv. 1/가운데 글자 가져오기.py
+++ Programmers/Lv. 1/가운데 글자 가져오기.py
@@ -1,8 +1,4 @@
 def solution(s):
     length = len(s)
 
-    return (
-        s[length // 2 - 1 : length // 2 + 1]
-        if length % 2 == 0
-        else s[length // 2]
-    )
+    return s[length // 2 - 1 : length // 2 + 1] if length % 2 == 0 else s[length // 2]
