9a6608cd04eb33062db04dad28edf4501b436331
KimJinung <KimJinung@outlook.com>
2022-12-16 20:43:10 +0900

    Optimization
    Remove duplication local variable

--- Programmers/Lv. 2/스킬트리.py
+++ Programmers/Lv. 2/스킬트리.py
@@ -1,29 +1,27 @@
 def solution(skill, skill_trees):
+    skills = skill
+
     answer = 0
 
+    prerequisite_dict = {}
+
+    for idx, skill in enumerate(skills):
+        prerequisite_dict[skill] = list(skills[:idx])
+
     for tree in skill_trees:
-        if is_valid_skill_tree(skill, tree):
+        if is_valid_skill_tree(prerequisite_dict, tree):
             answer += 1
 
     return answer
 
 
-def is_valid_skill_tree(order: str, skill_tree: str) -> bool:
-    skill_dict = {}
-
+def is_valid_skill_tree(prerequisites: dict, skill_tree: str) -> bool:
     skill_stack = []
 
-    for idx, skill in enumerate(order):
-
-        # Set prerequisite skills for current skill
-        skill_dict[skill] = list(order[:idx])
-
     for skill in skill_tree:
-        prerequisite = skill_dict.get(skill)
+        prerequisite = prerequisites.get(skill)
 
-        if prerequisite is not None:
-
-            # Check current prerequisite skill stack
+        if prerequisite is not None:  # Check current prerequisite skill stack
             if skill_stack != prerequisite:
                 return False
 
