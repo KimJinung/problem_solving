222305d4dca776279cd7eb1c89be25121c23e21d
KimJinung <KimJinung@outlook.com>
2022-12-17 18:28:33 +0900

    Modify function name for readability

--- Programmers/Lv. 2/[3차] n진수 게임.py
+++ Programmers/Lv. 2/[3차] n진수 게임.py
@@ -7,18 +7,18 @@ def solution(n, t, m, p):
     result = "0"
 
     for num in range(1, t * m):
-        result += get_radix(n, num)
+        result += radix_transformation(n, num)
 
     return "".join(result[idx] for idx in range(p - 1, t * m, m))
 
 
-def get_radix(n: int, number: int) -> str:
+def radix_transformation(base: int, number: int) -> str:
     result = ""
 
     while number != 0:
-        number, mod = divmod(number, n)
+        number, mod = divmod(number, base)
 
-        if n > 10 and (10 <= mod <= 15):
+        if base > 10 and (10 <= mod <= 15):
             result += "ABCDEF"[mod % 10]
         else:
             result += str(mod)
