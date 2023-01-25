37f09d7c041535a702a3e222717e841d9d865a51
KimJinung <KimJinung@outlook.com>
2023-01-26 00:59:01 +0900

    2023.01.26.

--- Programmers/Lv. 2/쿼드압축 후 개수 세기.py
+++ Programmers/Lv. 2/쿼드압축 후 개수 세기.py
@@ -1,60 +1,75 @@
-# 섹션 나누는 코드를 만들지 못했음
-# 섹션을 구하면 굳이 플래그 비트까지 사용할 필요가 없을듯?
+"""
+동일하게 반복되는 패턴 -> 재귀
+
+쿼드압축
+1. 2차원 배열이 압축 가능한지 확인한다.
+2. 압축 가능하면 업데이트 / 불가능하면 4개의 섹션으로 나누고 재귀에 태운다.
+"""
+
+answer = [0, 0]
+
+
 def solution(arr):
-    answer = [0, 0]
+    global answer
 
-    # 1. 초기값 설정
-    size = len(arr)
-
-    for subset in arr:
-        for bit in subset:
+    for row in arr:
+        for bit in row:
             answer[bit] += 1
 
-    # 2. 압축
-    n = size // 2
-
-    while n != 1:
-        sections = [
-            (0, n, 0, n),
-            (0, n, n, 2 * n),
-            (n, 2 * n, 0, n),
-            (n, 2 * n, n, 2 * n),
-        ]
-
-        for x_start, x_end, y_start, y_end in sections:
-            quad = arr[x_start][y_start]
-
-            if quad != -1:
-                if result := is_compress(x_start, x_end, y_start, y_end, arr):
-                    answer[quad] -= result - 1
-
-                    arr = set_complete(x_start, x_end, y_start, y_end, arr)
-
-        n //= 2
+    quad_compress(arr)
 
     return answer
 
 
-def is_compress(x_start, x_end, y_start, y_end, arr) -> int:
-    prev = arr[x_start][y_start]
+def quad_compress(arr):
+    global answer
 
+    size = len(arr)
+
+    if size < 2:
+        return
+
+    if result := is_compress(arr):
+        bit = arr[0][0]
+
+        answer[bit] -= result - 1
+    else:
+        size //= 2
+
+        sections = [
+            (0, size, 0, size),
+            (0, size, size, 2 * size),
+            (size, 2 * size, 0, size),
+            (size, 2 * size, size, 2 * size),
+        ]
+
+        next_sections = []
+
+        for x_st, x_ed, y_st, y_ed in sections:
+            section = []
+            for x in range(x_st, x_ed):
+                row = []
+                for y in range(y_st, y_ed):
+                    row.append(arr[x][y])
+
+                section.append(row)
+
+            next_sections.append(section)
+
+        for section in next_sections:
+            quad_compress(section)
+
+
+def is_compress(arr: list) -> int:
+    prev_bit = arr[0][0]
     cnt = 0
 
-    for x in range(x_start, x_end):
-        for y in range(y_start, y_end):
-            if prev != arr[x][y]:
+    for row in arr:
+        for bit in row:
+            if prev_bit != bit:
                 return 0
 
-            prev = arr[x][y]
-
+            prev_bit = bit
             cnt += 1
 
     return cnt
-
-
-def set_complete(x_start, x_end, y_start, y_end, arr) -> list:
-    for x in range(x_start, x_end):
-        for y in range(y_start, y_end):
-            arr[x][y] = -1
-
-    return arr
