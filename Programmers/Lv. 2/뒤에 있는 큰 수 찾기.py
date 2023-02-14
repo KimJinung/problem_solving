654f7edfd67deaa6d63978191f030481db7ffbd2
KimJinung <KimJinung@outlook.com>
2023-02-14 21:02:23 +0900

    2023.02.14.

---
+++ Programmers/Lv. 2/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,17 @@
+"""
+스택을 활용하는 문제
+"""
+
+
+def solution(numbers):
+    answer = [-1 for _ in range(len(numbers))]
+
+    stack = []
+
+    for idx, number in enumerate(numbers):
+        while stack and numbers[stack[-1]] < number:
+            answer[stack.pop()] = number
+
+        stack.append(idx)
+
+    return answer
