f940561e9e67de80a60a6610dc44158f5f1f84a2
KimJinung <KimJinung@outlook.com>
2023-02-14 18:48:05 +0900

    2023.02.14.

---
+++ Programmers/Lv. 4/도둑질.py
@@ -0,0 +1,22 @@
+def solution(sticker):
+    size = len(sticker)
+
+    if size <= 3:
+        return max(sticker)
+
+    return max(dp(sticker[:-1]), dp(sticker[1:]))
+
+
+def dp(sticker: list) -> int:
+    size = len(sticker)
+
+    dp = [0 for _ in range(size)]
+    dp[0] = sticker[0]
+
+    for idx in range(1, size):
+        if idx == 1:
+            dp[idx] = max(sticker[0], sticker[1])
+        else:
+            dp[idx] = max(dp[idx - 1], sticker[idx] + dp[idx - 2])
+
+    return dp[-1]
