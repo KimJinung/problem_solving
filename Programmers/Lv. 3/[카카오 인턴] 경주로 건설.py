7c5979aeac4f7ca9667ac282e2726e8a88fe9e8b
KimJinung <KimJinung@outlook.com>
2023-03-10 19:52:03 +0900

    2023.03.10.

---
+++ Programmers/Lv. 3/[카카오 인턴] 경주로 건설.py
@@ -0,0 +1,94 @@
+"""
+문제가 요구하는 것
+- 0, 0 좌표부터 n, n좌표까지 경로 중 최소 요금
+
+사용할 알고리즘
+- BFS
+
+아이디어
+- BFS로 탐색한다.
+- 현재 경로와 다음 경로에서 x값이 변하면 height로 마킹, y값이 변하면 width로 마킹한다.
+- 이동 경로와 이전 경로의 마킹 값이 다르면 코너이므로 600원을 더한다. 같다면 100원을 더한다.
+- 탐색하면서 이전에 탐색한 값보다 작은 사이즈로 체크할 수 있다면 업데이트 한다.
+
+아쉬운점
+- 테스트 케이스 25번의 경우 이미 업데이트가 끝난 이후에 더 비용이 높았던 경로가 나중에 비용이 더 저렴해지는 상황임
+- 나는 3차원 리스트를 사용하는 대신에 board의 초대 크기가 25 x 25인 것을 보고 reamind_chance 변수를 두어서 1000회까지는 비용이 더 많더라도 탐색할 기회를 주도록 해서 
+패스하게 만들었음.
+- 아마도 이 방법이 정해는 아닌 것 같음.. 더 좋은 방법 생각해보기
+
+"""
+from collections import deque
+
+
+def solution(board):
+    size = len(board) - 1
+
+    load = []
+
+    # Make load
+    for subset in board:
+        line = []
+        for item in subset:
+            if item == 0:
+                line.append("@")  # 3차원?
+            else:
+                line.append("#")
+
+        load.append(line)
+
+    # BFS
+    optimized_rotue_fee = bfs(load)
+
+    return optimized_rotue_fee[size][size]
+
+
+def bfs(load) -> (list, list):
+    size = len(load) - 1
+
+    Q = deque([[(0, 0), None, 0]])  # dots, prev dots, fee
+
+    remaind_chance = 1000
+
+    while Q:
+        dots, prev_direction, fee = Q.popleft()
+
+        row = dots[0]
+        col = dots[1]
+
+        is_possible = False
+
+        if load[row][col] == "@":
+            load[row][col] = fee
+            is_possible = True
+        elif fee <= load[row][col]:
+            load[row][col] = fee
+            is_possible = True
+        elif remaind_chance:
+            is_possible = True
+            remaind_chance -= 1
+
+        if is_possible:
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                next_row = row + dx
+                next_col = col + dy
+
+                direction = "w" if dx == 0 else "h"
+
+                if not prev_direction:
+                    next_fee = 100
+                elif prev_direction == direction:
+                    next_fee = 100
+                else:
+                    next_fee = 600
+
+                if (
+                    0 <= next_row <= size
+                    and 0 <= next_col <= size
+                    and load[next_row][next_col] != "#"
+                ):
+                    Q.append([(next_row, next_col), direction, fee + next_fee])
+
+            is_possible = False
+
+    return load
